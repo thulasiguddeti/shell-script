0ad0171ec4a2125dab5b7d6bef64b165f6b80b1b
thulasiguddeti <thulasiguddeti02@gmail.com>
2024-08-08 13:03:53 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -3,8 +3,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "Error :: Please run the script with root acess"
+    exit 1 # you can give other than zero
 else
     echo "Your are a root user"
-fi
+fi # it indicates if condition ends here.
 
 yum install mysql -y
