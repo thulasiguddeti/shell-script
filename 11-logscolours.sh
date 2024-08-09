efde6a6e5b8bbd0c4141f2a5d749b64882548e9a
thulasiguddeti <thulasiguddeti02@gmail.com>
2024-08-09 12:15:33 +0530

    Scripts

---
+++ 11-logscolours.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+ID=$(id -u) 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "Error ::  $2 .... $R failed $N"
+        exit 1
+    else
+        echo " $2..... $G success $N"
+    fi 
+}
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error :: Please run the script with root access $N"
+    exit 1 # you can give other than zero
+else
+    echo "Your are a root user"
+fi # it indicates if condition ends here.
+
+yum install mysql -y &>> $LOGFILE
+
+VALIDATE $?  "Installing mysql"
+
+yum install git -y  &>> $LOGFILE
+
+VALIDATE  $?  "Installing Git"
