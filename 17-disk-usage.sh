3c456ca9dfcd011930e9145b75899ec3b0036a4e
thulasiguddeti <thulasiguddeti02@gmail.com>
2024-08-19 12:52:53 +0530

    shell scripts

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -9,10 +9,10 @@ while IFS= read line
 do
 
     usage=$(echo $line | awk '{print$6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print$1F}'
+    partition=$(echo $line | awk '{print$1F}')
     if [ $usage -gt $DISK_USAGE ]
     then
-        message+="High Disk Usage on : $usage"
+        message+="High Disk Usage on $partition: $usage"
     fi 
 
 done <<< $DISK_USAGE
